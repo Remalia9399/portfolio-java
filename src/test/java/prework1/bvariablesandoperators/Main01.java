c5102b02b629d0d53afa3fb8143378d1540df975
Remalia9399 <remalia9399@gmail.com>
2024-05-20 15:14:26 +0200

    Add prework1.bvariablesandoperators

---
+++ src/test/java/prework1/bvariablesandoperators/Main01.java
@@ -0,0 +1,18 @@
+package prework1.bvariablesandoperators;
+
+public class Main01 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		int int1 = 1;
+		char char2 = 'c';
+		long long3 = -2 ^ 60;
+		double double4 = 12.0;
+		boolean boolean5 = true;
+//        System.out.println(int1);
+//        System.out.println(char2);
+//        System.out.println(long3);
+//        System.out.println(double4);
+//        System.out.println(boolean5);
+	}
+}
