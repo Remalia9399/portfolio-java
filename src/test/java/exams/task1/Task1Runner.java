53e32dd5485d44f3f52a2ab95dd88a6d8dd3bd91
Remalia9399 <remalia9399@gmail.com>
2024-05-25 16:27:34 +0200

    before change all readme

--- src/test/java/exams/task1/Task1Runner.java
+++ src/test/java/exams/task1/Task1Runner.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/java/exams/task1/feature/task1.feature",
-        glue = "src/test/java/exams/task1/Task1.java"
+//        glue = "src/test/java/exams/task1/Task1.java",
+        plugin = {"pretty", "html:reports/cucumber-report-task1.html"}
 )
 public class Task1Runner {
 
