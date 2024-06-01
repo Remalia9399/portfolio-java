4de035b223a4153d93c45253966c436957f367c5
Remalia9399 <remalia9399@gmail.com>
2024-06-01 21:48:32 +0200

    add example ExceptionExample2

--- src/test/java/basic2/example/ExceptionExample2.java
+++ src/test/java/basic2/example/ExceptionExample2.java
@@ -1,2 +1,23 @@
-package basic2.example;public class ExceptionExample2 {
+package basic2.example;
+
+public class ExceptionExample2 {
+
+    public static void main(String[] args) {
+        int a = 16;
+//        int b = 0;
+//        int b = -1;
+        int b = 3;
+        System.out.println(divide(a, b));
+    }
+
+    static int divide(int a, int b) throws ArithmeticException {
+        if (b == 0) {
+            throw new IllegalArgumentException("cannot be divided by 0");
+        }
+        if (b == -1) {
+            IllegalArgumentException myExceptionInstance = new IllegalArgumentException(" |-1| ");
+            throw myExceptionInstance;
+        }
+        return a / b;
+    }
 }
