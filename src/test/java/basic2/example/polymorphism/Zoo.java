3caa94b1732678442c4ef41f814bdb03e3d07376
Remalia9399 <remalia9399@gmail.com>
2024-06-02 14:21:19 +0200

    .. add exception return/continue/break

--- src/test/java/basic2/example/polymorphism/Zoo.java
+++ src/test/java/basic2/example/polymorphism/Zoo.java
@@ -25,6 +25,8 @@ public class Zoo {
         for (Animal a : this.animals) {
             if(a instanceof Dog) {
                 a.feed(new Meat());
+//                break;
+//                return;
                 continue;
             }
             if(a instanceof Whale) {
