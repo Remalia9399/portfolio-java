e3a182d266d62312f3512fa94e6032aa954c070d
Remalia9399 <remalia9399@gmail.com>
2024-07-01 16:07:46 +0200

    add.. options to cucumber src/test/java/exams/task1/Task1Runner.java

--- src/test/java/exams/task1/Task1Runner.java
+++ src/test/java/exams/task1/Task1Runner.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+//        fetrues = add file or folder
         features = "src/test/java/exams/task1/feature/task1.feature",
 //        glue = "src/test/java/exams/task1/Task1.java",
 //        plugin = {"pretty", "html:out"}
